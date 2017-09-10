6a482a908a374c0cf1f35d411cb1b2be5d5207ff
rymcmahon <toedragdangle9@gmail.com>
2017-09-09 22:28:00 -0400

    added barrooms form partial and wired up new barroom form and show

--- app/controllers/barrooms_controller.rb
+++ app/controllers/barrooms_controller.rb
@@ -1,2 +1,28 @@
 class BarroomsController < ApplicationController
+  def show
+    @barroom = Barroom.find(params[:id])
+  end
+  def new
+    @barroom = Barroom.new
+  end
+
+  def create
+    @barroom = Barroom.new(barroom_params)
+    respond_to do |format|
+      if @barroom.save
+        @barroom.barroom_users.where(user_id: current_user.id).first_or_create
+        format.html { redirect_to @barroom, notice: 'Barroom was successfully created.' }
+        format.json { render :show, status: :created, location: @barroom }
+      else
+        format.html { render :new }
+        format.json { render json: @barroom.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    def barroom_params
+      params.require(:barroom).permit(:name)
+    end
 end
