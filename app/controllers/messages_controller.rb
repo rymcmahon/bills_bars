c071b40f7cbac326481c6804d7a06115e2cce00d
rymcmahon <toedragdangle9@gmail.com>
2017-09-11 21:41:46 -0400

    built up new message form and show

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,21 @@
+class MessagesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_barroom
+
+  def create
+    message = @barroom.messages.new(message_params)
+    message.user = current_user
+    message.save
+    redirect_to @barroom
+  end
+
+  private
+
+    def set_barroom
+      @barroom = Barroom.find(params[:barroom_id])
+    end
+
+    def message_params
+      params.require(:message).permit(:body)
+    end
+end
