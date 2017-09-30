b6d3f6277c4a9539ef8ee885ef9356d15f5e6c65
rymcmahon <toedragdangle9@gmail.com>
2017-09-30 19:51:31 -0400

    got unreads to stop appearing in sender window

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -3,10 +3,9 @@ class DirectMessagesController < ApplicationController
 
   def show
     users = [current_user, User.find(params[:id])]
-    @barroom = Barroom.direct_message_for_users.for_users(users)
+    @barroom = Barroom.direct_message_for_users(users)
     @messages = @barroom.messages.order(created_at: :desc).limit(100).reverse
     @barroom_user = current_user.barroom_users.find_by(barroom_id: @barroom.id)
-    @current_barroom = current_user.barroom_users.where(barroom_id: @barroom.id)
     render "barrooms/show"
   end
 
--- app/models/barroom.rb
+++ app/models/barroom.rb
@@ -1,14 +1,14 @@
 class Barroom < ApplicationRecord
-  has_many :barroom_users
+  has_many :barroom_users, dependent: :destroy
   has_many :users, through: :barroom_users
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   scope :public_channels, ->{ where(direct_message: false) }
   scope :direct_messages, ->{ where(direct_message: true) }
 
   def self.direct_message_for_users(users)
-    users_ids = users.map(&:id).sort
-    name = "DM:#{users_ids.join(":")}"
+    user_ids = users.map(&:id).sort
+    name = "DM:#{user_ids.join(":")}"
 
     if barroom = direct_messages.where(name: name).first
       barroom
