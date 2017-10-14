aea159e2078b5d9e16316970412a172b2282a920
rymcmahon <toedragdangle9@gmail.com>
2017-10-13 20:46:35 -0400

    added smaller wordmark img

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def news
-    feed = Feedjira::Feed.fetch_and_parse 'https://www.google.com/alerts/feeds/00315441985827146858/9510803395009003313'
+    feed = Feedjira::Feed.fetch_and_parse 'https://www.google.com/alerts/feeds/00315441985827146858/14937607138872366122'
     @entries = feed.entries
     @barrooms = Barroom.all
   end
