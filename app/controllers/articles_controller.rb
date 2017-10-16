5e031e278ae50d6ce96d72493e65f9776ac00a2d
Ryan McMahon <rmcmahon@OSX-RMCMAHO-MBP.local>
2017-10-16 13:47:11 -0400

    added group by day to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.all.order('created_at ASC')
+    @daily_articles = @articles.group_by(&:day)
     @barrooms = Barroom.all
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  def day
+    self.created_at.strftime('%B %d, %Y')
+  end
 end
